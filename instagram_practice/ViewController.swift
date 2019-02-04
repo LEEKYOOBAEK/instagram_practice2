20f07cb28c1f86e4d95dc2396a9b4631bb3d2f39
LEEKYOOBAEK <charisma1124@gmail.com>
2019-02-04 19:02:27 +0900

    Initial Commit

---
+++ instagram_practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instagram_practice
+//
+//  Created by MBP01 on 04/02/2019.
+//  Copyright © 2019 MBP01. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
